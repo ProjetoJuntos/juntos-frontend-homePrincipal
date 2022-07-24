b00306287fc1f7bb94ec732edfb5062864ddb6cb
rodrigolsc <rodrigo.laugeni@hotmail.com>
2022-07-24 18:00:03 -0300

    Integrando api login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,59 +2,84 @@ import React from "react";
 import { useState } from 'react';
 import juntos_logo from '../../img/juntos_logo.png';
 import style from '../../css/loginStyle.module.css';
+import authService from "../../services/auth.service";
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+class Login extends React.Component{
 
-  return (
-    <div id='login'>
-    <div className={style.container}>
-      <div className={style.containerLogin}>
-        <div className={style.wrapLogin}>
-          <form className={style.loginForm}>
-            <span className={style.loginFormTitle}> Bem vindo </span>
+  constructor(props){
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
 
-            <span className={style.loginFormTitle}>
-              <img src={juntos_logo} alt="Juntos Logo" />
-            </span>
+  sendLogin = async (event) => {
+    event.preventDefault();
+    let data = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    try {
+      let res = await authService.authenticate(data)
+      console.log("res", res.data)
+      authService.setLoggedUser(res.data)
+    } catch (error) {
+      console.log("error", error)
+      alert("Email ou senha incorretos")
+    }
+  }
 
-            <div className={style.wrapInput}>
-              <input
-                className={style.input}
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <span className={style.focusInput} data-placeholder="Email"></span>
-            </div>
+  render() {
+    return (
+      <div id='login'>
+      <div className={style.container}>
+        <div className={style.containerLogin}>
+          <div className={style.wrapLogin}>
+            <form onSubmit={this.sendLogin} className={style.loginForm}>
+              <span className={style.loginFormTitle}> Bem vindo </span>
 
-            <div className={style.wrapInput}>
-              <input
-                className={style.input}
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <span className={style.focusInput} data-placeholder="Password"></span>
-            </div>
+              <span className={style.loginFormTitle}>
+                <img src={juntos_logo} alt="Juntos Logo" />
+              </span>
 
-            <div className={style.containerLoginFormBtn}>
-              <button className={style.loginFormBtn}>Login</button>
-            </div>
+              <div className={style.wrapInput}>
+                <input
+                  className={style.input}
+                  type="email"
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                />
+                <span className={style.focusInput} data-placeholder="Email"></span>
+              </div>
 
-            <div className={style.textCenter}>
-              <span className={style.txt1}>Não possui conta? </span>
-              <a className={style.txt2} href="#">
-                Criar conta
-              </a>
-            </div>
-          </form>
+              <div className={style.wrapInput}>
+                <input
+                  className={style.input}
+                  type="password"
+                  value={this.state.password}
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                />
+                <span className={style.focusInput} data-placeholder="Password"></span>
+              </div>
+
+              <div className={style.containerLoginFormBtn}>
+                <button className={style.loginFormBtn}>Login</button>
+              </div>
+
+              <div className={style.textCenter}>
+                <span className={style.txt1}>Não possui conta? </span>
+                <a className={style.txt2} href="#">
+                  Criar conta
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-// import { url } from '../Url';
+// import axios from 'axios';
+// // import { url } from '../Url';
 
 
-const Api = axios.create({ baseURL: 'https://juntos-srv.herokuapp.com/api' });
- //const Api = axios.create({ baseURL: 'http://localhost:8080/' });
+// const Api = axios.create({ baseURL: 'https://juntos-srv.herokuapp.com/api' });
+//  //const Api = axios.create({ baseURL: 'http://localhost:8080/' });
 
-export default Api;
+// export default Api;
